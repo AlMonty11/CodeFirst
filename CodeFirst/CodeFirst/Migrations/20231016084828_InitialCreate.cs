ff9c0b8bb32f9c5f99e5caea9e4f233bc23a3f3e
Alberto <jalberto1991@hotmail.com>
2023-10-16 09:56:10 +0100

    Creación DDBB a través de consola: Add-Migration

---
+++ CodeFirst/CodeFirst/Migrations/20231016084828_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeFirst.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Car",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Note = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ExamDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Car", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Car");
+        }
+    }
+}
